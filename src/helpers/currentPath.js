12df0d2ba1cc2f4f9e7699e0758e1193e0551fa7
shiroe66 <shiroeqky@gmail.com>
2022-06-10 00:26:32 +0500

    feat: helper file

---
+++ src/helpers/currentPath.js
@@ -0,0 +1,18 @@
+const dir = process.env.HOME || process.env.USERPROFILE
+const array = dir.split("\u005C")
+
+export const currentPath = (action = "", path) => {
+  if (action.trim() === "") {
+    return `${array.join("\u005C")}\u005C`
+  }
+
+  if (action === "up" && array.length > 1) {
+    array.pop()
+    return array.join("\u005C")
+  }
+
+  if (action === "cd") {
+    array.push(path)
+    return array.join("\u005C")
+  }
+}
