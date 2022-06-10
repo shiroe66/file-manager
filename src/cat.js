4dca5cdbafcddbee18b8a94aa0fdb65b5fc2aecf
shiroe66 <shiroeqky@gmail.com>
2022-06-10 23:42:20 +0500

    fix: promise to streams

--- src/cat.js
+++ src/cat.js
@@ -1,11 +1,11 @@
-import fs from "fs/promises"
+import { createReadStream } from "fs"
 import path from "path"
 import { currentPath } from "./helpers/currentPath.js"
 
 export const cat = async (input) => {
   const file = path.join(currentPath(), input.slice(3).trim())
 
-  fs.readFile(file, "utf-8")
-    .then((value) => console.log(value))
-    .catch((err) => console.error(err))
+  const rs = createReadStream(file)
+  rs.on("data", (chunk) => console.log(chunk.toString()))
+  rs.on("error", () => console.log("Operation failed"))
 }
