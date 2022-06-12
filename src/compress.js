5b6c0c195fe8ceadec677264f13da981b703dff4
shiroe66 <shiroeqky@gmail.com>
2022-06-12 23:02:38 +0500

    fix: added 2nd arg

--- src/compress.js
+++ src/compress.js
@@ -3,18 +3,15 @@ import { createBrotliCompress } from "zlib"
 import { join } from "path"
 
 export const compress = async (input) => {
-  const [, file] = input.split(" ")
+  const [, file, path] = input.split(" ")
   const source = join(process.cwd(), file)
-
-  const [newFile] = file.split(".")
-  const dest = join(process.cwd(), `${newFile}.br`)
+  const dest = join(`${path}.br`)
 
   const rs = createReadStream(source)
   const ws = createWriteStream(dest)
-  const brotli = createBrotliCompress()
+  rs.on("error", () => console.log("Operation failed"))
+  ws.on("error", () => console.log("Operation failed"))
 
-  const stream = rs.pipe(brotli).pipe(ws)
-  stream.on("finish", () =>
-    console.log(`Compressing done! Your filename ${newFile}`)
-  )
+  const brotli = createBrotliCompress()
+  rs.pipe(brotli).pipe(ws)
 }
