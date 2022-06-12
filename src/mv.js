4f26b3bd0cff9d5ad401a1c69d429bc7c6eaff8f
shiroe66 <shiroeqky@gmail.com>
2022-06-12 13:43:48 +0500

    removed currentPath func

--- src/mv.js
+++ src/mv.js
@@ -1,13 +1,12 @@
 import { createReadStream, createWriteStream } from "fs"
 import { join } from "path"
-import { currentPath } from "./helpers/currentPath.js"
 
 export const mv = async (input) => {
   const [, file, folder] = input.split(" ")
 
   try {
-    const filePath = join(currentPath(), file)
-    const folderPath = join(currentPath(), folder)
+    const filePath = join(process.cwd(), file)
+    const folderPath = join(process.cwd(), folder)
 
     if (filePath && folderPath) {
       const rs = createReadStream(filePath)
