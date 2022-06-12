75aa2c6239f3d4fde83831ac90befa3ada8364ec
shiroe66 <shiroeqky@gmail.com>
2022-06-12 15:37:38 +0500

    fix: stat

--- src/cd.js
+++ src/cd.js
@@ -1,14 +1,11 @@
-import { readdir } from "fs/promises"
+import { stat } from "fs/promises"
 
-export const cd = async (input) => {
+export const cd = (input) => {
   const folder = input.slice(3)
-  const folders = await (await readdir(process.cwd(), { withFileTypes: true }))
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => dirent.name)
-  const find = folders.find((dirent) => dirent === folder)
-  if (find) {
-    process.chdir(`./${folder}`)
-  } else {
-    console.error("Operation failed")
-  }
+
+  stat(`${process.cwd()}\\${folder}`, (_, stats) => {
+    if (stats.isDirectory()) {
+      process.chdir(`./${folder}`)
+    }
+  })
 }
