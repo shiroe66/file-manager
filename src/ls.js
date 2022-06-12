9a0d35a2d50cd52846f17b0f53248bf08ebc60b8
rasul <shiroeqky@gmail.com>
2022-06-12 16:32:01 +0500

    fix: call function

--- src/ls.js
+++ src/ls.js
@@ -1,7 +1,7 @@
 import { readdir } from "fs/promises"
 
 export const ls = async () => {
-  return readdir(process.cwd)
-    .then((list) => table(list))
+  return readdir(process.cwd())
+    .then((list) => console.table(list))
     .catch((err) => console.error(err))
 }
