49aedf1e594721600c41998dd0a534d10e3dbb01
shiroe66 <shiroeqky@gmail.com>
2022-06-12 22:08:37 +0500

    feat: move file

--- src/mv.js
+++ src/mv.js
@@ -1,19 +1,13 @@
-import { createReadStream, createWriteStream } from "fs"
+import { rename } from "fs/promises"
 import { join } from "path"
 
 export const mv = async (input) => {
   const [, file, folder] = input.split(" ")
 
-  try {
-    const filePath = join(process.cwd(), file)
-    const folderPath = join(process.cwd(), folder)
+  const filePath = join(process.cwd(), file)
+  const folderPath = join(process.cwd(), folder, file)
 
-    if (filePath && folderPath) {
-      const rs = createReadStream(filePath)
-      const ws = createWriteStream(folderPath)
-      rs.pipe(ws)
-    }
-  } catch (e) {
+  await rename(filePath, folderPath).catch(() =>
     console.log("Operation failed")
-  }
+  )
 }
